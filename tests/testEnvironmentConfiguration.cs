6852864ad50f973cc347769d1c58646c46a00e36
jwintersbro <joshuawintersbrown@gmail.com>
2022-11-11 12:52:04 -0500

    Added Test case of iEnvironmentConfigurations and EnvironmentConfigurations

---
+++ tests/testEnvironmentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using no_bueno.emailManager;
+using System.Text.Json;
+
+namespace no_bueno.tests
+{
+    public class testEnvironmentConfiguration
+    {
+        public testEnvironmentConfiguration()
+        {
+            var myEnvironmentConfiguration = new environmentConfiguration();
+            Console.WriteLine($"\r\nTesting initial get :{myEnvironmentConfiguration.smtpPassword()}");
+            Console.WriteLine($"Testing Updating the Value :{myEnvironmentConfiguration.smtpPassword("ive changed the password!")}");
+            Console.WriteLine($"Checking that update value stuck :{myEnvironmentConfiguration.smtpPassword()}");
+        }
+    }
+}
+
+
+
