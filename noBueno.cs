917642a80dac9da769c0e5eb077b9dcdd8776ea0
jwintersbro <joshuawintersbrown@gmail.com>
2022-11-11 10:30:53 -0500

    Final naming style

---
+++ noBueno.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var debugging = true;
+
+        Console.WriteLine("Starting no bueno...");
+
+        if (debugging)
+        {
+
+            /// Run test email controller.
+            var myEmailController = new no_bueno.testEmailController();
+        }
+    }
+
+}
--- no_bueno.cs
+++
@@ -1,9 +0,0 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Starting...");
-        var instance = new no_bueno.emailController();
-    }
-
-}
